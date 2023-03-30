58ef17194723b207706ed32f6ef72c96e164f752
Patrick R <gnss1985@gmail.com>
2023-03-30 14:59:22 +0200

    Update icon-star.svg, illustration-thank-you.svg, and 15 more files...

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 import "./../scss/style.scss";
+
+let selectedValue;
+const submitButton = document.querySelector(".button");
+const allButtons = document.querySelectorAll(".score");
+
+allButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    selectedValue = this.value;
+
+    allButtons.forEach((ratingButton) => {
+      ratingButton.classList.remove("active");
+    });
+
+    this.classList.add("active");
+  });
+});
+
+submitButton.addEventListener("click", () => {
+  if (selectedValue) {
+    document.querySelector("#vote").style.display = "none";
+    document.querySelector("#result").style.display = "flex";
+    document.querySelector(".rating--result").innerHTML =
+      "You selected " + selectedValue + " out of 5";
+  }
+});
